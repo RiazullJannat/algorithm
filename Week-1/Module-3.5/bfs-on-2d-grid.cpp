444b814eae93c8d9d92956b4416f80468f1677a2
RJannat <ramimk097@gmail.com>
2025-08-21 12:44:58 +0600

    shorted distence

--- Week-1/Module-3.5/bfs-on-2d-grid.cpp
+++ Week-1/Module-3.5/bfs-on-2d-grid.cpp
@@ -4,6 +4,7 @@ int n, e;
 char adjList[105][105];
 bool visited[105][105] = {false};
 vector<pair<int, int>> routes = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+int level[105][105] = {-1};
 bool valid(int r, int c)
 {
     return (r > -1 && r < n && c > -1 && c < e);
@@ -13,12 +14,12 @@ void bfs(int r, int c)
     queue<pair<int, int>> q;
     q.push({r, c});
     visited[r][c] = true;
+    level[r][c] = 0;
     while (!q.empty())
     {
         pair<int, int> top = q.front();
         int pr = top.first;
         int pc = top.second;
-        cout << pr << " - " << pc << endl;
         q.pop();
         for (int i = 0; i < 4; i++)
         {
@@ -28,6 +29,7 @@ void bfs(int r, int c)
             {
                 visited[cr][cc] = true;
                 q.push({cr, cc});
+                level[cr][cc] = level[pr][pc] + 1;
             }
         }
     }
@@ -38,8 +40,9 @@ int main()
     for (int i = 0; i < n; i++)
         for (int j = 0; j < e; j++)
             cin >> adjList[i][j];
-    int r, c;
-    cin >> r >> c;
+    int r, c, dr, dc;
+    cin >> r >> c >> dr >> dc;
     bfs(r, c);
+    cout << level[dr][dc];
     return 0;
 }
