facf0f09ac10298f2e0fc501bf0ca13311cb51fa
RJannat <ramimk097@gmail.com>
2025-08-22 19:56:20 +0600

    solved another problem using graph

---
+++ Week-1/module-4(exam)/Connected_or_Not.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int arr[n][n];
+    memset(arr, 0, sizeof(arr));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i == j)
+                arr[i][j] = 1;
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        arr[a][b] = 1;
+    }
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (arr[a][b])
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
--- Week-1/module-4(exam)/Count_Apartments.cpp
+++ Week-1/module-4(exam)/Count_Apartments.cpp
@@ -1,14 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-int row, col;
+int n, e;
 char matrix[1001][1001];
 int visited[1001][1001];
 vector<pair<int, int>> routes = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 bool valid(int r, int c)
 {
-    return r > -1 && r < row && c > -1 && c < col;
+    return (r > -1 && r < n && c > -1 && c < e);
 }
-int main() {
-    
+int dfs(int row, int col)
+{
+    visited[row][col] = true;
+    int count = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int cRow, cCol;
+        cRow = row + routes[i].first;
+        cCol = col + routes[i].second;
+        if (valid(cRow, cCol) && !visited[cRow][cCol] && matrix[cRow][cCol] == '.')
+        {
+            count += dfs(cRow, cCol);
+        }
+    }
+    return count;
+}
+int main()
+{
+    memset(visited, false, sizeof(visited));
+    cin >> n >> e;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < e; j++)
+            if (!visited[i][j] && matrix[i][j] == '.')
+            {
+                dfs(i, j);
+                sum++;
+            }
+    cout << sum << endl;
     return 0;
 }
