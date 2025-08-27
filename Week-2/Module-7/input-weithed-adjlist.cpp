9e7ede60e963a5ecc7a624ff09a40fbdaa1820c3
RJannat <ramimk097@gmail.com>
2025-08-27 12:04:31 +0600

    store undirected and directed weidthed list in adjlist

---
+++ Week-2/Module-7/input-weithed-adjlist.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<pair<int, int>> adjList[n];
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adjList[a].push_back({b, c});
+        adjList[b].push_back({a, c});
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> ";
+        for (auto p : adjList[i])
+        {
+            cout << p.first << " " << p.second << ",";
+        }
+        cout << endl;
+    }
+    return 0;
+}
