1bb2642ed77d159676491957ca6296a725b878ab
RJannat <ramimk097@gmail.com>
2025-09-04 15:19:38 +0600

    find DSU using recurshion

--- Week-3/Module-11/find-using-recurshion.cpp
+++ Week-3/Module-11/find-using-recurshion.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+int par[101];
 
-int main() {
-    
+int find(int src)
+{
+    if (par[src] == -1)
+        return src;
+    int leader = find(par[src]);
+    return leader;
+}
+int main()
+{
+    memset(par, -1, sizeof(par));
+    par[0] = 1;
+    par[2] = 1;
+    par[3] = 1;
+    par[4] = 5;
+    par[5] = 3;
+    cout << find(4) << endl;
     return 0;
 }
