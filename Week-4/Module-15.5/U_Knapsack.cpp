626a005df961263381ea3e01fb559f544f624675
RJannat <ramimk097@gmail.com>
2025-09-14 19:21:23 +0600

    solved problem again

---
+++ Week-4/Module-15.5/U_Knapsack.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int value[1001], weight[1001];
+int dp[1001][1001];
+int knapsack(int n, int w)
+{
+    if (n < 0 || w <= 0)
+        return 0;
+    if (dp[n][w] != -1)
+        return dp[n][w];
+    if (weight[n] <= w)
+    {
+        int opt1 = knapsack(n - 1, w - weight[n]) + value[n];
+        int opt2 = knapsack(n - 1, w);
+        dp[n][w] = max(opt1, opt2);
+        return dp[n][w];
+    }
+    else
+    {
+        dp[n][w] = knapsack(n - 1, w);
+        return dp[n][w];
+    }
+}
+int main()
+{
+    int n, w;
+    cin >> n >> w;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i] >> value[i];
+    }
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= w; j++)
+            dp[i][j] = -1;
+    cout << knapsack(n, w) << endl;
+    return 0;
+}
